b4adf2b5cafb1fe22a22225d68e65de52ede0565
kevorteg <kevin@somosvocesqueinspiran.org>
2026-07-14 20:56:26 -0500

    Add cdn.sanity.io to next/image remotePatterns

--- web/next.config.ts
+++ web/next.config.ts
@@ -14,6 +14,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "*.private.blob.vercel-storage.com",
       },
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+      },
     ],
   },
   async headers() {
